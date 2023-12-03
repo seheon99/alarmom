c7f45f9846176ee48b330103fe8a1335376cdc45
Seheon Yu <49232918+seheon99@users.noreply.github.com>
2023-12-03 21:39:49 +0900

    Add alarm initialization and provider scope to main()

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,19 @@
 // Flutter imports:
 import 'package:flutter/material.dart';
 
+// Package imports:
+import 'package:alarm/alarm.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
 // Project imports:
 import 'package:alarmom/presentation/app.dart';
 
-void main() {
-  runApp(const App());
+void main() async {
+  await Alarm.init();
+
+  runApp(
+    const ProviderScope(
+      child: App(),
+    ),
+  );
 }
